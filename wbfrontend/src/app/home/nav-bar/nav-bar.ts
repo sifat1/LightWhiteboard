b412df75afc30046d080584508153cafc9e25bce
sifat1 <sifatshikdhar@gmail.com>
2025-09-09 20:07:35 +0600

    fixed ui

--- wbfrontend/src/app/home/nav-bar/nav-bar.ts
+++ wbfrontend/src/app/home/nav-bar/nav-bar.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav-bar.html',
   styleUrls: ['./nav-bar.css']
 })
 export class NavBar {
-
+  islogged: boolean = false;
 }
